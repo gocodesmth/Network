21c364edad42e676077e976ec8213f3d9d1736e4
User <sherikjumayev1202@gmail.com>
2023-07-05 19:12:00 +0500

    Changes: Lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello, samurai! Let's go!
-      <ul>
-        <li>html</li>
-        <li>css</li>
-        <li>js</li>
-        <li>react</li>
-      </ul>
+    <div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
 
 export default App;
+
+const Technologies = () => {
+  return (
+    <div>
+        <ul>
+          <li>html</li>
+          <li>css</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div>
+      <a href='#s'> Home</a>
+      <a href='#s'>News Feed</a>
+      <a href='#s'>Messages</a>
+    </div>
+  )
+}
