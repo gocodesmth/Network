200a4d3dd429f5c12bd9508bbafa64d10d4d5b70
User <sherikjumayev1202@gmail.com>
2023-07-05 18:41:56 +0500

    Changes: Lesson 6

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
-     <ul>
-      <li>html</li>
-      <li>css</li>
-      <li>js</li>
-      <li>react</li>
-     </ul>
+      Hello, samurai! Let's go!
+      <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
     </div>
   );
 }
