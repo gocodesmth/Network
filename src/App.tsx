5c8e45469d21064019741b613d78b13be456ed62
User <sherikjumayev1202@gmail.com>
2023-07-05 17:20:46 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello, samurai! Let's go!
+     <ul>
+      <li>html</li>
+      <li>css</li>
+      <li>js</li>
+      <li>react</li>
+     </ul>
+    </div>
+  );
+}
+
+export default App;
